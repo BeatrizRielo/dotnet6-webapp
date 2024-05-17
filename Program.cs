f6c33bef53d29992ba6546b27267c37860528c2a
divanov <divanov@Marias-MacBook-Air.local>
2024-05-17 09:58:46 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace MyWebApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, 8080);
+                })
+                .Configure(app =>
+                {
+                    app.Run(async context =>
+                    {
+                        await context.Response.WriteAsync("OK, this works");
+                    });
+                })
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
